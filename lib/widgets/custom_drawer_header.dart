1a15b52d9f9f7f090107b3647381ff0caf398b9d
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2020-11-06 17:07:09 +0500

    Updates

--- lib/widgets/custom_drawer_header.dart
+++ lib/widgets/custom_drawer_header.dart
@@ -16,52 +16,55 @@ class CustomDrawerHeader extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       child: Column(
         children: [
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Weather Today',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+          Container(
+            height: 150,
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Weather Today',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        data == null ? 'Loading...' : '${data.temp}',
-                        //'-1',
-                        style:data == null ? TextStyle(fontSize: 20):const TextStyle(fontSize: 60),
-                      ),
-                      Text(
-                        data == null ? '' : '°C',
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              Expanded(
-                child: data == null
-                    ? FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: const Center(
-                          child: Text(' '),
-                          //CircularProgressIndicator(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          data == null ? 'Loading...' : '${data.temp}',
+                          //'-1',
+                          style:data == null ? TextStyle(fontSize: 20):const TextStyle(fontSize: 60),
                         ),
-                      )
-                    : Image.network(
-                        'http:' + data.iconImageUrl,
-                        fit: BoxFit.cover,
-                      ),
-              ),
-            ],
+                        Text(
+                          data == null ? '' : '°C',
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: data == null
+                      ? FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: const Center(
+                            child: Text(' '),
+                            //CircularProgressIndicator(),
+                          ),
+                        )
+                      : Image.network(
+                          'http:' + data.iconImageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+              ],
+            ),
           ),
           LocationSelector(
             textSize: 22,
